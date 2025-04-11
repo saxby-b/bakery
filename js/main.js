dddcbe6d31e8de4f262e37c3c14af4151e803423
C. Saxby Bain <csaxbybain@yahoo.com>
2025-04-11 15:14:53 -0400

    Uppdated light and dark modes event

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const body = document.querySelector("body");
 const headerContent = document.querySelector(".header-content");
 const headerDivider = document.querySelector(".header-divider");
 const navContent = document.querySelector(".nav-content");
+const about = document.querySelector("#about");
+const a = document.querySelector("a:link");
 
 
 const update = document.querySelector(".update span");
@@ -16,10 +18,12 @@ modeList.addEventListener("change", function (e) {
     mood = e.target.value;
 
     if (mood === "dark") {
+        about.style.setProperty('background-color', '#2e2e1f');
         body.classList.add("dark-mode-body");
         headerContent.classList.add("dark-mode-header-content");
         headerDivider.classList.add("dark-mode-divider");
         navContent.classList.add("dark-mode-dark-gray");
+        a.classList.add("dark-mode");
        
 
     } else if (mood === "light") {
@@ -27,6 +31,8 @@ modeList.addEventListener("change", function (e) {
         headerContent.classList.remove("dark-mode-header-content");
         headerDivider.classList.remove("dark-mode-divider");
         navContent.classList.remove("dark-mode-dark-gray");
+        about.style.setProperty('background-color', '#dedece');
+        a.classList.add("dark-mode");
         
         
 
