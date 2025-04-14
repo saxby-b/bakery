26a4d6c40b6ea410251d581fd7c21c5230cee7b1
C. Saxby Bain <csaxbybain@yahoo.com>
2025-04-14 15:20:17 -0400

    Updated events

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ const headerContent = document.querySelector(".header-content");
 const headerDivider = document.querySelector(".header-divider");
 const navContent = document.querySelector(".nav-content");
 const about = document.querySelector("#about");
-const a = document.querySelector("a:link");
+const li = document.querySelector("li"); 
+const catering = document.querySelector("#catering");
 
 
 const update = document.querySelector(".update span");
@@ -19,20 +20,22 @@ modeList.addEventListener("change", function (e) {
 
     if (mood === "dark") {
         about.style.setProperty('background-color', '#2e2e1f');
-        body.classList.add("dark-mode-body");
+        body.classList.add("dark-mode-general");
         headerContent.classList.add("dark-mode-header-content");
         headerDivider.classList.add("dark-mode-divider");
         navContent.classList.add("dark-mode-dark-gray");
-        a.classList.add("dark-mode");
+        li.classList.add("dark-mode-general");
+        catering.style.setProperty('background-color', '#2e2e1f');
        
 
     } else if (mood === "light") {
-        body.classList.remove("dark-mode-body");
+        body.classList.remove("dark-mode-general");
         headerContent.classList.remove("dark-mode-header-content");
         headerDivider.classList.remove("dark-mode-divider");
         navContent.classList.remove("dark-mode-dark-gray");
         about.style.setProperty('background-color', '#dedece');
         a.classList.add("dark-mode");
+        catering.style.setProperty('background', '#dedece');
         
         
 
